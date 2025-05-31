14db7f64239be88362b6d6e7c15a027e2db93130
Lobito271 <jorge.zavaleta211@gmail.com>
2025-05-31 17:26:17 -0500

    adiós react mejora en hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';  // <-- Importa React para Astro
+
 
 export default defineConfig({
-  integrations: [react()],  // <-- Agrega el integrador React aquí
+  integrations: [],  
   vite: {
     plugins: [tailwindcss()]
   },
