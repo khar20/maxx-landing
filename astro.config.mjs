f145e66ba48159cc969080a08ae2fc61aa04bc0d
khar20 <136043524+khar20@users.noreply.github.com>
2025-10-07 00:58:06 -0500

    fixed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import deno from "@deno/astro-adapter";
+import react from '@astrojs/react';
 
 
 export default defineConfig({
